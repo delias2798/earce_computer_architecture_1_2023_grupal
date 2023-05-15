d84f293df689229f8018456ddacb42dccff524e4
delias2798 <32471383+delias2798@users.noreply.github.com>
2023-05-15 03:54:40 -0600

    [FIX] Flags ALU and some testbenchs
    Range of values allowed [-2147483648,2147483648]

--- TessiaV1/testbench/ALU_tb.sv
+++ TessiaV1/testbench/ALU_tb.sv
@@ -23,7 +23,7 @@ module ALU_tb();
 
     // Test cases
     initial begin
-        // Test case 1: Addition (5 + 3 = 8) **********************************
+        // Test case 1: Addition (1 + 1 = 2) **********************************
         a = 4'b0001;
         b = 4'b0001;
         ctrl = 4'b0000;
@@ -31,26 +31,92 @@ module ALU_tb();
         assert(result === 4'b0010) else $error("Test case 1 failed");
         assert(flags === 4'b0000) else $error("Test case 1 failed");
 		  
-		  // Test case 2: Addition ***********************************************
+		  // Test case 2: Addition (-1 + 1 = temp_result = 0000) ***********************************************
+        a = 4'b1111;
+        b = 4'b0001;
+		  //b = 4'b1110;
+        ctrl = 4'b0000;
+        #10;
+		  
+		  // 3=negative, 2=zero, 1=carry, 0=overflow
+        assert(result === 4'b0000) else $error("Test case 2 failed (result)");
+        assert(flags === 4'b0100) else $error("Test case 2 failed (flags)");
+		  
+		  // Test case 3: Addition (temp_result = 1 1101) ***********************************************
         a = 4'b1111;
         b = 4'b1110;
         ctrl = 4'b0000;
         #10;
 		  
 		  // 3=negative, 2=zero, 1=carry, 0=overflow
-        assert(result === 4'b1101) else $error("Test case 2 failed (result)");
-        assert(flags === 4'b1010) else $error("Test case 2 failed (flags)");
+        assert(result === 4'b1101) else $error("Test case 3 failed (result)");
+        assert(flags === 4'b1000) else $error("Test case 3 failed (flags)");
 		  
-		  
-		  // Test case 3: Addition ***********************************************
-        a = 4'b0000;
-        b = 4'b1110;
+		  // Test case 4: Addition (-1 + 0 temp_result = -1 = 1111) ***********************************************
+        a = 4'b1111;
+        b = 4'b0000;
         ctrl = 4'b0000;
         #10;
+
+		  // 3=negative, 2=zero, 1=carry, 0=overflow
+        assert(result === 4'b1111) else $error("Test case 4 failed (result)");
+        assert(flags === 4'b1000) else $error("Test case 4 failed (flags)");
+		  
+		  // Test case 5: Addition (7 + 4 = 11 Overfloat and carrie) ***********************************************
+        a = 4'b0111;
+        b = 4'b0100;
+        ctrl = 4'b0000;
+        #10;
+		  $display("Binary a: %0b",a);
+		  $display("Binary b: %0b",b);
+		  $display("Binary result: %0b",result);
+		  $display("Binary flags: %0b",flags);
+		  
 		  
 		  // 3=negative, 2=zero, 1=carry, 0=overflow
-        assert(result === 4'b1110) else $error("Test case 3 failed (result)");
-        assert(flags === 4'b1000) else $error("Test case 3 failed (flags)");
+        assert(result === 4'b1011) else $error("Test case 5 failed (result)");
+        assert(flags === 4'b0011) else $error("Test case 5 failed (flags)");
+		  
+		  // Test case 6: Addition (7 + 4 = 11 Overfloat and carrie) ***********************************************
+        a = 4'b1111;
+        b = 4'b1100;
+        ctrl = 4'b0000;
+        #10;
+		  $display("Binary a: %0b",a);
+		  $display("Binary b: %0b",b);
+		  $display("Binary result: %0b",result);
+		  $display("Binary flags: %0b",flags);
+		  
+		  
+		  // 3=negative, 2=zero, 1=carry, 0=overflow
+        assert(result === 4'b1011) else $error("Test case 6 failed (result)");
+        assert(flags === 4'b0011) else $error("Test case 6 failed (flags)");
+		  /*
+		  // Test case 6: Substraction (temp_result = 1) ***********************************************
+        a = 4'b0010;
+        b = 4'b0001;
+        ctrl = 4'b0001;
+        #10;
+		  
+		  
+		  // 3=negative, 2=zero, 1=carry, 0=overflow
+        assert(result === 4'b0001) else $error("Test case 6 failed (result)");
+        assert(flags === 4'b0000) else $error("Test case 6 failed (flags)");
+		  
+		  // Test case 7: Substraction (temp_result = - 0001) ***********************************************
+        a = 4'b0010;
+        b = 4'b0011;
+        ctrl = 4'b0001;
+        #10;
+		  $display("Binary a: %0b",a);
+		  $display("Binary b: %0b",b);
+		  $display("Binary result: %0b",result);
+		  $display("Binary flags: %0b",flags);
+		  
+		  
+		  // 3=negative, 2=zero, 1=carry, 0=overflow
+        assert(result === 4'b0001) else $error("Test case 7 failed (result)");
+        assert(flags === 4'b1000) else $error("Test case 7 failed (flags)");
 		  
 		  // Test case 4: Addition ************************************************
         a = 4'b1111;
@@ -59,9 +125,9 @@ module ALU_tb();
         #10;
 		  
 		  // 3=negative, 2=zero, 1=carry, 0=overflow
-        assert(result === 4'b1110) else $error("Test case 4 failed (result)");
-        assert(flags === 4'b1010) else $error("Test case 4 failed (flags)");
-		  
+        //assert(result === 4'b1110) else $error("Test case 4 failed (result)");
+        //assert(flags === 4'b1010) else $error("Test case 4 failed (flags)");
+		  */
 		  
 
         $display("All test cases passed");
