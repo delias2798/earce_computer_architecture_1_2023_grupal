b2397686b299b33866467ec18071a39c944ecb4d
Kevin Acevedo Rodriguez <keviinar51@gmail.com>
2023-05-15 18:41:45 -0600

    Guide Script Complete

--- high-level-scripts/rippling.py
+++ high-level-scripts/rippling.py
@@ -2,37 +2,40 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-# Leer la imagen
-A = cv2.imread('barbara.jpg')
-A = cv2.resize(A, (128, 128))
+# Leer la imagen y convertirla a escala de grises
+A = cv2.imread('barbara.jpg', cv2.IMREAD_GRAYSCALE)
+A = cv2.resize(A, (300, 300))
 plt.subplot(1, 2, 1)
-plt.imshow(cv2.cvtColor(A, cv2.COLOR_BGR2RGB))
+plt.imshow(A, cmap='gray')
 
 # Extraemos los parámetros
-m, n, r = A.shape
+m, n = A.shape
 
 # Período y amplitudes
 Lx = 75
 Ly = 75
 
 # Primer frame
-Y = np.zeros((m, n, r, 41), dtype=np.uint8)
-Y[:, :, :, 0] = A
+Y = np.zeros((m, n, 41), dtype=np.uint8)
+Y[:, :, 0] = A
 
-for k in range(5, 201, 5):
+for k in range(5, 200, 5):
     Ax = k
     Ay = k
     # Genera una imagen en blanco
-    B = np.zeros((m, n, r), dtype=np.uint8)
+    B = np.zeros((m, n), dtype=np.uint8)
     for x in range(m):
         for y in range(n):
+            # Calcular las nuevas coordenadas para el pixel actual
             xnew = np.mod(round(x + Ax * np.sin(2 * np.pi * y / Lx)), m)
             xaux = round(x + Ax * np.sin(2 * np.pi * y / Lx))
             ynew = np.mod(round(y + Ay * np.sin(2 * np.pi * x / Ly)), n)
             yaux = round(y + Ay * np.sin(2 * np.pi * x / Ly))
+            # Validar los rangos de la imagen
             if (xnew == xaux) and (ynew == yaux):
-                B[xnew, ynew, :] = A[x, y, :]
+                B[xnew, ynew] = A[x, y]
     plt.subplot(1, 2, 2)
-    plt.imshow(cv2.cvtColor(B, cv2.COLOR_BGR2RGB))
-    plt.pause(0.1)
-    Y[:, :, :, k // 5] = B
+    plt.imshow(B, cmap='gray')
+    # Delay por un corto periodo de tiempo
+    plt.pause(0.01)
+    Y[:, :, k // 5] = B
